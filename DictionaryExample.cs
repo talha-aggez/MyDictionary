35b4ae7150ad2f6dd11473093ef92514ade7911d
talha-aggez <66861181+talha-aggez@users.noreply.github.com>
2021-02-20 10:29:10 +0300

    MyDictionary

---
+++ DictionaryExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryExample
+{
+    public class DictionaryExample<TKey , TValue >
+    {
+        TKey[] _key;
+        TValue[] _value;
+        public DictionaryExample()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKey = _key;
+            TValue[] tempValue = _value;
+
+            _value = new TValue[_value.Length + 1];
+            _key = new TKey[_key.Length + 1];
+
+            for(int i = 0; i < tempKey.Length; i++)
+            {
+                _value[i] = tempValue[i];
+                _key[i] = tempKey[i];
+            }
+            _value[_value.Length - 1] = value;
+            _key[_key.Length - 1] = key;
+        }
+        public void List()
+        {
+            for(int i = 0; i < _value.Length; i++)
+            {
+                Console.WriteLine("{0}, {1}",_key[i],_value[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryExample<int, string> brandList = new DictionaryExample<int, string>();
+
+            brandList.Add(1, "BMW");
+            brandList.Add(2, "Honda");
+
+            brandList.List();
+        }
+    }
+}
